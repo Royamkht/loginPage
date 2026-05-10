347017582e038fe27b4f7fef5cf90d595319bc88
Royamkht <royamokhtari8066@gmail.com>
2026-05-10 14:46:24 +0330

    rename job

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -7,14 +7,14 @@ test('login by valid email and password', async ({ api, config }) => {
     const response = await api
         .path('login')
         .body({ username: config.userName, password: config.Passsword })
-        .postRequest(400)
+        .postRequest(200)
     const getToken = response.ok
     const username = response.user.username
     expect(getToken === true).toBeTruthy()
     console.log("welcome", username)
 })
 
-test('loginssssss by invalid password (less than 8 characters)', async ({ api, config }) => {
+test('login by invalid password (less than 8 characters)', async ({ api, config }) => {
     const response = await api
         .path('login')
         .body({ username: config.userName, password: "123" })
